b8bd2bf11668a9b8d34e86bd9942e0f7551e9e47
Mercykylax <nyawiramercy49@gmail.com>
2019-04-11 15:08:23 +0300

    This is my python work

---
+++ CALC.py
@@ -0,0 +1,30 @@
+def add(a,b):
+	c=a+b
+	print(c)
+
+
+def subtract(a,b):
+    c=a-b
+    print(c)
+
+def multiply(b,c):
+    d=b*c
+    print(d)
+
+def divide(b,c):
+	d=b/c
+	print(d)
+def exponent(b,c):
+	d=b**c
+    print(d)
+def modulus(g,h):
+    i=g%h
+    print(i)
+def percentage(g,h):
+    i=g/h*100
+    print(i)
+def whole-number-division(g,h):
+    i=g//h
+    print(i)
+
+        	        
